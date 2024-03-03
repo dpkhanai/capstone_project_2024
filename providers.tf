47fcb4fde3f560cd8b518081270e784f70c1522e
dpkhanai <156655109+dpkhanai@users.noreply.github.com>
2024-03-03 14:52:20 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = “AKIA5FTZBFB7JSVZSAET”
-  secret_key = “zPM3G9/gqgv4eRjcSk8Pix9jnERtV4U0TVm72WEt”
+  access_key = "AKIA5FTZBFB7JSVZSAET"
+  secret_key = "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"
 }
